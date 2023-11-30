c67e3899d8f1f3230fe85a320cf27532ba2429c8
Barış Şaraldı <brsrld@gmail.com>
2023-11-30 19:34:30 +0300

    project init

---
+++ BombNews/BombNews/BombNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombNewsApp.swift
+//  BombNews
+//
+//  Created by Barış Şaraldı on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BombNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BombNews/BombNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BombNews
+//
+//  Created by Barış Şaraldı on 30.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
